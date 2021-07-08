48103bb365e6bee7b0b303b666792346e7954351
Nirav Prajapati <nirav.prajapati.p@pimcore.services>
2021-07-08 09:54:14 +0530

    completing state mgmt in square screen

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -8,11 +8,28 @@ const SquareScreen = () => {
 	const [red, setRed] = useState(0);
 	const [green, setGreen] = useState(0);
 	const [blue, setBlue] = useState(0);
+
+	const setColor = (color, change) => {
+		switch(color){
+			case 'red':
+				red + change > 255 || red + change < 0 ? null : setRed(red + change);
+				return;
+			case 'green':
+				green + change > 255 || green + change < 0 ? null : setGreen(green + change);
+				return;
+			case 'blue':
+				blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change);
+				return;
+			default:
+				return;
+		}
+	}
+
 	return (
 		<View>
-			<ColorCounter onIncrease={() => setRed(red + COLOR_INC)} onDecrease={() => setRed(red - COLOR_INC)} color="Red" />
-			<ColorCounter onIncrease={() => setGreen(green + COLOR_INC)} onDecrease={() => setGreen(green - COLOR_INC)} color="Green" />
-			<ColorCounter onIncrease={() => setBlue(blue + COLOR_INC)} onDecrease={() => setBlue(blue - COLOR_INC)} color="Blue" />
+			<ColorCounter onIncrease={() => setColor('red', COLOR_INC)} onDecrease={() => setColor('red', -1 * COLOR_INC)} color="Red" />
+			<ColorCounter onIncrease={() => setColor('green', COLOR_INC)} onDecrease={() => setColor('green', -1 * COLOR_INC)} color="Green" />
+			<ColorCounter onIncrease={() => setColor('blue', COLOR_INC)} onDecrease={() => setColor('blue', -1 * COLOR_INC)} color="Blue" />
 			<View style={{ width: 100, height: 100, backgroundColor: `rgb(${red},${green},${blue})` }} />
 		</View>
 	)
